ca35a31b388ecfb20f210ac9f5ec6e9d4e2fbf03
Michal <khakaimic4@gmail.com>
2023-07-01 16:32:33 +0300

    completed lab

--- app/controllers/leases_controller.rb
+++ app/controllers/leases_controller.rb
@@ -4,6 +4,33 @@ class LeasesController < ApplicationController
         render json: leases, status: :ok
     end
 
-    def show
+    def create
+      lease = Lease.new(lease_params)
+      if lease.save
+        render json: lease, status: :created
+      else
+        render json: { errors: lease.errors.full_messages }, status: :unprocessable_entity
+      end
     end
-end
+  
+    def destroy
+      lease = find_lease
+      if lease
+        lease.destroy
+        head :no_content
+      else
+        render json: { error: "Oops! Lease not found" }, status: :not_found
+      end
+    end
+  
+    private
+  
+    def find_lease
+      Lease.find_by(id: params[:id])
+    end
+  
+    def lease_params
+      params.permit(:apartment_id, :tenant_id, :rent)
+    end
+  end
+  
